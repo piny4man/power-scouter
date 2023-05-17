bd81e4ee53b302cbcf15f10af726e04bd7a2695d
piny4man <development@pinya.dev>
2023-05-17 09:13:29 +0200

    Add competitor info to calculated scores + Fix radio buttons click

--- front/src/components/score_competitor.rs
+++ front/src/components/score_competitor.rs
@@ -1,19 +1,27 @@
 use dioxus::prelude::*;
 
 #[inline_props]
-pub fn ScoreCompetitor<'a>(
-    cx: Scope<'a>,
+pub fn ScoreCompetitor(
+    cx: Scope,
     gendre: String,
-    category: &'a str,
-    movements: &'a str,
-    unit: &'a str,
-    body_weight: &'a f64,
-    lifted_weight: &'a f64,
+    category: String,
+    movements: String,
+    unit: String,
+    body_weight: f64,
+    lifted_weight: f64,
 ) -> Element {
     cx.render(rsx!(header {
         class: "score-competitor",
-        span {
-            "{gendre} {category} {movements} {body_weight:.2}{unit} {lifted_weight:.2}{unit}"
+        h4 {
+            "{gendre} {category} {movements}"
+        }
+        aside {
+            span {
+                "Body {body_weight:.2} {unit}"
+            }
+            span {
+                "Lifted {lifted_weight:.2} {unit}"
+            }
         }
     }))
 }
