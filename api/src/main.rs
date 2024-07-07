31308b611312e40f6847bf8326bf40bf369a015c
piny4man <development@pinya.dev>
2024-07-07 21:12:53 +0200

    fix: actix address

--- api/src/main.rs
+++ api/src/main.rs
@@ -18,8 +18,8 @@ async fn calculate_results(competitor_info: Json<CompetitorInfo>) -> Result<Json
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let address = "127.0.0.1:8080";
-    println!("Starting server to {address}");
+    let address = "0.0.0.0:8080";
+    println!("Starting server to {}", address);
 
     HttpServer::new(|| {
         App::new()
